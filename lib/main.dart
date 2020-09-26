b54eca9ff2a2cddca4943a5a194da191584ee4b3
ThienejLee <thienvanlea1@gmail.com>
2020-09-26 18:46:32 +0700

    init

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'package:ChatApp/views/signup.dart';
+import 'package:flutter/material.dart';
+
+import 'views/signin.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      title: 'Chat App demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primaryColor: Color(0xff145C9E),
+        scaffoldBackgroundColor: Color(0xff1F1F1F),
+        primarySwatch: Colors.blue,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: SignUp(),
+    );
+  }
+}
---
+++ lib/views/signup.dart
@@ -0,0 +1,107 @@
+import 'package:ChatApp/widgets/widget.dart';
+import 'package:flutter/material.dart';
+
+class SignUp extends StatefulWidget {
+  @override
+  _SignUpState createState() => _SignUpState();
+}
+
+class _SignUpState extends State<SignUp> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: appBarMain(context),
+      body: SingleChildScrollView(
+        child: Container(
+          height: MediaQuery.of(context).size.height - 50,
+          alignment: Alignment.bottomCenter,
+          child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 24),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  TextField(
+                      style: simpleTextStyle(),
+                      decoration: textFieldInputDecoration("username")),
+                  TextField(
+                      style: simpleTextStyle(),
+                      decoration: textFieldInputDecoration("email")),
+                  TextField(
+                      style: simpleTextStyle(),
+                      decoration: textFieldInputDecoration("psasword")),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Container(
+                    alignment: Alignment.centerRight,
+                    child: Container(
+                      padding:
+                          EdgeInsets.symmetric(horizontal: 16, vertical: 8),
+                      child: Text(
+                        "Forget Password? ",
+                        style: simpleTextStyle(),
+                      ),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        gradient: LinearGradient(colors: [
+                          const Color(0xff007EF4),
+                          const Color(0xff2A75BC),
+                        ]),
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text(
+                      "Sign Up",
+                      style: mediumTextStyle(),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8.0,
+                  ),
+                  Container(
+                    alignment: Alignment.center,
+                    width: MediaQuery.of(context).size.width,
+                    padding: EdgeInsets.symmetric(vertical: 20),
+                    decoration: BoxDecoration(
+                        color: Colors.white,
+                        borderRadius: BorderRadius.circular(30)),
+                    child: Text(
+                      "Sign Up with Google",
+                      style: TextStyle(color: Colors.black87, fontSize: 17),
+                    ),
+                  ),
+                  SizedBox(
+                    height: 16.0,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Already have account",
+                        style: mediumTextStyle(),
+                      ),
+                      Text(
+                        "SignIn now",
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 17,
+                            decoration: TextDecoration.underline),
+                      )
+                    ],
+                  ),
+                  SizedBox(
+                    height: 50,
+                  ),
+                ],
+              )),
+        ),
+      ),
+    );
+  }
+}
